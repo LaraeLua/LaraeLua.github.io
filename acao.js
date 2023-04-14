11d6ad8cdba0b48727bc24d748171fd4e940a58a
LaraeLua <111380182+LaraeLua@users.noreply.github.com>
2023-04-13 23:28:50 -0300

    Update acao.js

--- acao.js
+++ acao.js
@@ -1,13 +1,12 @@
-
-    
 function ProjetoDesenv(el){    
     
     var display = document.getElementById(el).style.display;
-    var designinfo = document.querySelector(".designdiv");
+    var designinfo = document.getElementById("Designinfo2");
     var design = document.querySelector(".Design");
     var robotica = document.querySelector(".Robotica");
     var roboticainfo = document.querySelector(".Roboticainfo");
     var footer = document.querySelector("footer");
+
         if (display == "none"){
 
             footer.style.top = "400%";
@@ -16,41 +15,48 @@ function ProjetoDesenv(el){
             design.style.top = "302%";
             designinfo.style.top = "302%";
             document.getElementById(el).style.display = 'block';
-
         }else{
-            footer.style.top = "400%";
+            footer.style.top = "360%";
             robotica.style.top = "302%";
             roboticainfo.style.top = "302%";
-            design.style.top = "500%";
-            designinfo.style.top = "500%";
+            design.style.top = "261%";
+            designinfo.style.top = "261%";
             document.getElementById(el).style.display = 'none';
         }
 }
 
-function ProjetoDesign(el){
+function ProjetoDesign(el){    
+    
     var display = document.getElementById(el).style.display;
-
-    var design = document.querySelector(".Design");
     var robotica = document.querySelector(".Robotica");
-    var roboticainfo = document.querySelector(".Roboticainfo");
+    var roboticainfo = document.getElementById("robot2");
     var footer = document.querySelector("footer");
-
         if (display == "none"){
 
             footer.style.top = "400%";
             robotica.style.top = "343%";
             roboticainfo.style.top = "343%";
-            design.style.top = "400%";
-            designinfo.style.top = "302%";
             document.getElementById(el).style.display = 'block';
 
         }else{
-            footer.style.top = "400%";
+            footer.style.top = "360%";
             robotica.style.top = "302%";
             roboticainfo.style.top = "302%";
-            design.style.top = "400%";
-            designinfo.style.top = "400%";
             document.getElementById(el).style.display = 'none';
         }
-  }
+}
 
+function ProjetoRobot(el){    
+    
+    var display = document.getElementById(el).style.display;
+    var footer = document.querySelector("footer");
+        if (display == "none"){
+
+            footer.style.top = "405%";
+            document.getElementById(el).style.display = 'block';
+
+        }else{
+            footer.style.top = "360%";
+            document.getElementById(el).style.display = 'none';
+        }
+}
